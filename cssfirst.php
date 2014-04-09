340993018654881258c8c7bc8fd63ce8f0c796ae
Anirudh <anirudh7@live.com>
2014-04-09 14:58:50 +1200

    Create cssfirst.php

---
+++ cssfirst.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+* Created using PhpStorm.
+* User: anirudhmerugu
+* Date: 7/04/14
+* Time: 11:52 AM
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.plugin.plugin');
+jimport('joomla.environment.response');
+
+class plgSystemCssFirst extends JPlugin 
+{
+    function onContentPrepare($context, &$row, &$params, $page = 0)
+    {
+        $document = JFactory::getDocument();
+        $document->addStyleSheet(JURI::base(). "plugins/system/cssfirst/load.css");
+        $document->addScript(JURI::base(). "plugins/system/cssfirst/load.js");
+    }
+    function onAfterRender()
+    {
+        $response = JResponse::getBody();
+        $response = JString::str_ireplace('<body class="', '<body class="load ', $response);
+        JResponse::setBody($response);
+    }
+}
