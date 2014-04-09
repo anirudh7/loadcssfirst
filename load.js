8a8361d7d6812b8b55555312b5194874a6618927
Anirudh <anirudh7@live.com>
2014-04-09 14:54:11 +1200

    Create load.js

---
+++ load.js
@@ -0,0 +1,7 @@
+/**
+ * Created by anirudhmerugu on 7/04/14.
+ */
+window.addEventListener('load',function load() {
+    window.removeEventListener('load', load, false);
+    document.body.classList.remove('load');
+},false);
